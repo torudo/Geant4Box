d8fd00554139f42f5244ba293543ba7d3e241a34
torudo <tobysantwort@gmail.com>
2019-07-03 19:43:30 +0200

    up

--- extract_data.py
+++ extract_data.py
@@ -10,18 +10,45 @@ f = ROOT.TFile.Open('build/Edep.root', 'read')
 canvas = ROOT.TCanvas('canvas', '', 500, 500)
 hist = f.Get('Edep')
 length = hist.GetEntries()
-#canvas.SaveAs('plot.pdf')
-#bins = [hist.GetSize()]
-#for i in xrange(len(bins)):#
-#    print(bins[i])
-bins = hist.GetArray()
-x=[]
-print(bins)
-for i in xrange(0,len(bins)):
-    x.append(bins[i])
-print(x)
 
+#int k = h->GetXaxis()->FindBin(x);
+x=[]
+for k in xrange(0,100):
+    x.append(hist.GetBinContent(k))
+
+print(x)
 f.Close()
+exit()
+#bins = hist.GetArray()
+#x=[]
+#print(bins)
+#for i in xrange(0,len(bins)):
+#    x.append(bins[i])
+
+x = ROOT.RooRealVar("x","x",-20,30);
+mpv = ROOT.RooRealVar("mpv","mpv",1,0.1,4);
+width = ROOT.RooRealVar("width","width",1,0.1,10);
+landau = ROOT.RooLandau("ld","ld",x,mpv,width);
+mean = ROOT.RooRealVar("mean","mean",0.1,0.1,4);
+sigma = ROOT.RooRealVar("sigma","sigma",0.5,0.1,10);
+gauss = ROOT.RooGaussian("gs","gs",x,mean,sigma);
+lxg = ROOT.RooFFTConvPdf("lxg","lxg",x,landau,gauss);
+
+#TestData
+data = lxg.generate(ROOT.RooArgSet(x),10000)
+
+result = gauss.fitTo(data,ROOT.RooFit.PrintLevel(-1))
+resultL = landau.fitTo(data,ROOT.RooFit.PrintLevel(-1))
+result_lxg = lxg.fitTo(data,ROOT.RooFit.PrintLevel(-1))
+
+xframe = x.frame()
+data.plotOn(xframe,ROOT.RooFit.Binning(25))
+gauss.plotOn(xframe)
+sigma.setVal(0.2)
+landau.plotOn(xframe,ROOT.RooFit.LineColor(2))
+lxg.plotOn(xframe,ROOT.RooFit.LineColor(3))
+
+xframe.Draw()
 
 
 #Getting the data in the Tree
